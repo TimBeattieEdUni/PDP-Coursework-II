b0001f2df75ce68f438983f6fe5d8849277c3f6e
Tim Beattie <tim@timthemighty.com>
2015-04-03 13:30:56 +0100

    Squash this; fixing compiler errors.

--- src/Squirrel.cpp
+++ src/Squirrel.cpp
@@ -144,24 +144,23 @@ namespace Biology
 		}
 		
 		//  let interested parties know
-		Pdp::eSquirrelStep::eSquirrelStep step;
+		Pdp::ESquirrelStep::eSquirrelStep step;
 		if (new_cell != m_cur_cell)
 		{			
 			//  tell old cell we've left
-			step = Pdp::eSquirrelStep::eOut;
-			MPI_BSend(&step, 1, MPI_INT, m_cur_cell + 2, Pdp::EMpiMsgTag::eSquirrelStep, m_comm.GetComm())
+			step = Pdp::ESquirrelStep::eOut;
+			MPI_Bsend(&step, 1, MPI_INT, m_cur_cell + 2, Pdp::EMpiMsgTag::eSquirrelStep, m_comm.GetComm())
 			
 			//  tell new cell we've arrived
-			step = Pdp::eSquirrelStep::eIn;
-			MPI_BSend(&step, 1, MPI_INT, new_cell + 2, Pdp::EMpiMsgTag::eSquirrelStep, m_comm.GetComm())
+			step = Pdp::ESquirrelStep::eIn;
+			MPI_Bsend(&step, 1, MPI_INT, new_cell + 2, Pdp::EMpiMsgTag::eSquirrelStep, m_comm.GetComm())
 		}
 		else
 		{
 			//  tell current cell we've stepped within it
-			step = Pdp::eSquirrelStep::eWithin;
-			MPI_BSend(&step, 1, MPI_INT, m_cur_cell + 2, Pdp::EMpiMsgTag::eSquirrelStep, m_comm.GetComm())
+			step = Pdp::ESquirrelStep::eWithin;
+			MPI_Bsend(&step, 1, MPI_INT, m_cur_cell + 2, Pdp::EMpiMsgTag::eSquirrelStep, m_comm.GetComm())
 		}
-		MPI_BSend(&, 1, MPI_INT, new_cell + 2, Pdp::EMpiMsgTag::eSquirrelStep, m_comm.GetComm())
 		m_cur_cell = new_cell;
 	}
 
